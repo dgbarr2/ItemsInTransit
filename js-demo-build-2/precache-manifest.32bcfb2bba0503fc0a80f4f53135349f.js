d94d8e09d4e1aec90225c98d1870f5918debd6bf
dgbarr2 <dgbarr@mac.com>
2019-07-11 16:18:15 +0100

    Add files via upload

---
+++ js-demo-build-2/precache-manifest.32bcfb2bba0503fc0a80f4f53135349f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea646f198919878b16aa1dcffcedd8a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e90c93e98dc1ea6b5688",
+    "url": "/static/css/main.a9092cbe.chunk.css"
+  },
+  {
+    "revision": "374284f81cfdc8418c4c",
+    "url": "/static/js/2.957e0a62.chunk.js"
+  },
+  {
+    "revision": "e90c93e98dc1ea6b5688",
+    "url": "/static/js/main.9a434bfd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a45566b3321fb89443c0fa71bb01d06d",
+    "url": "/static/media/cubicSVG.a45566b3.svg"
+  },
+  {
+    "revision": "e95145b0d0314bd92d1cbd528d5f6afc",
+    "url": "/static/media/exponentialSVG.e95145b0.svg"
+  },
+  {
+    "revision": "816ab47d8c9d9fc0d0d202d7666516dd",
+    "url": "/static/media/gaussianSVG.816ab47d.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b91cf72c8db612c0db65549a180255bd",
+    "url": "/static/media/quadraticSVG.b91cf72c.svg"
+  },
+  {
+    "revision": "608c770b3d6047dbf396a58274485768",
+    "url": "/static/media/xSquared_2.608c770b.svg"
+  }
+]);
